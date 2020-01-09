a1fd4551270a9677f14a14cbcf81b99dc8fa5ac7
Clinton Yeboah <clintonyeb@gmail.com>
2020-01-09 02:11:41 +0000

    More customization

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,14 +1,14 @@
 (function($) {
   "use strict";
-  
+
     $.fn.andSelf = function() {
       return this.addBack.apply(this, arguments);
     }
-  
+
       /* Loader Code Start */
-      $(window).on("load", function() { 
+      $(window).on("load", function() {
           $(".section-loader").fadeOut("slow");
-          
+
           var $container = $('.portfolioContainer');
           $container.isotope({
               filter: '*',
@@ -16,11 +16,11 @@
                   queue: true
               }
           });
-       
+
           $('.portfolio-nav li').click(function(){
               $('.portfolio-nav .current').removeClass('current');
               $(this).addClass('current');
-       
+
               var selector = $(this).attr('data-filter');
               $container.isotope({
                   filter: selector,
@@ -32,19 +32,19 @@
           });
         });
       /* Loader Code End */
-  
-   
+
+
     /*
     |====================
     | Mobile NAv trigger
     |=====================
     */
-    
+
     var trigger = $('.navbar-toggler'),
       overlay     = $('.overlay'),
       navc     = $('.navbar-collapse'),
       active      = false;
-  
+
 
       $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
           $('.navbar-toggler').toggleClass('active')
@@ -52,31 +52,38 @@
         //   $('.navbar-collapse').toggleClass('show');
           overlay.toggleClass('active');
           navc.toggleClass('active');
-      });  
-      
-        
+      });
+
+
     /*
     |=================
     | Onepage Nav
     |================
     */
-        
+
       $('#mh-header').onePageNav({
-          currentClass: 'active', 
+          currentClass: 'active',
           changeHash: false,
           scrollSpeed: 750,
           scrollThreshold: 0.5,
       });
-    
+
+  $('#mh-home').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+  });
+
     /*
     |=================
     | fancybox
     |================
     */
- 
+
       $("[data-fancybox]").fancybox({});
-      
-      
+
+
     /*
     |===============
     | WOW ANIMATION
@@ -86,16 +93,16 @@
           mobile: false  // trigger animations on mobile devices (default is true)
       });
       wow.init();
-      
-      
+
+
     /*
     |=================
     | AOS
     |================
-    */      
-      
+    */
+
       //AOS.init();
-  
+
     /*
     | ==========================
     | NAV FIXED ON SCROLL
@@ -109,32 +116,32 @@
             $(".nav-scroll").removeClass("nav-strict");
         }
     });
-    
+
 
     /*
     |=================
     | Progress bar
     |================
-    */   
+    */
     $(".determinate").each(function(){
       var width = $(this).text();
       $(this).css("width", width)
         .empty()
-        .append('<i class="fa fa-circle"></i>');                
+        .append('<i class="fa fa-circle"></i>');
     });
-    
+
     /*
     |=================
     | Portfolio mixin
     |================
-    */   
+    */
     $('#portfolio-item').mixItUp();
-    
+
     /*
     |=================
     | Client review
     |================
-    */   
+    */
      $('#mh-client-review').owlCarousel({
         loop: false,
         responsiveClass: true,
@@ -156,13 +163,13 @@
             items: 3,
           }
         }
-    });  
-    
+    });
+
     /*
     |=================
     | Project review slide
     |================
-    */   
+    */
     $('.mh-project-testimonial').owlCarousel({
         loop: true,
         responsiveClass: true,
@@ -186,13 +193,13 @@
             items: 1,
           }
         }
-    });     
-    
+    });
+
     /*
     |=================
     | Single Project review
     |================
-    */   
+    */
     $('#single-project').owlCarousel({
         loop: false,
         responsiveClass: true,
@@ -216,13 +223,13 @@
             items: 1,
           }
         }
-    });    
-    
+    });
+
     /*
     |=================
     | Project review slide
     |================
-    */   
+    */
     $('.mh-single-project-slide-by-side').owlCarousel({
         loop: false,
         responsiveClass: true,
@@ -247,13 +254,13 @@
             items: 1,
           }
         }
-    });     
-    
+    });
+
     /*
     |=================
     | Single client review
     |================
-    */   
+    */
     $('#mh-single-client-review').owlCarousel({
         loop: false,
         responsiveClass: true,
@@ -275,13 +282,13 @@
             items: 1,
           }
         }
-    });   
-    
+    });
+
     /*
     |=================
     | Clint review slide
     |================
-    */   
+    */
     $('#mh-2-client-review').owlCarousel({
         loop: false,
         responsiveClass: true,
@@ -304,8 +311,8 @@
           }
         }
     });
-    
-    
+
+
     // Smooth Scroll
         // $(function() {
         //   $('a[href*=#]:not([href=#])').click(function() {
@@ -321,15 +328,15 @@
         //     }
         //   });
         // });
-        
-        
-        
+
+
+
     /*
     |=================
     | CONTACT FORM
     |=================
     */
-        
+
       $("#contactForm").validator().on("submit", function (event) {
           if (event.isDefaultPrevented()) {
             // handle the invalid form...
@@ -341,7 +348,7 @@
             submitForm();
           }
        });
-    
+
         function submitForm(){
           var name = $("#name").val();
           var email = $("#email").val();
@@ -364,7 +371,7 @@
             $("#contactForm")[0].reset();
             submitMSG(true, "Message Sent!")
         }
-    	  function formError(){   
+    	  function formError(){
     	    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
     	        $(this).removeClass();
     	    });
@@ -377,7 +384,7 @@
           }
           $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
         }
-    
 
-    
+
+
 }(jQuery));
